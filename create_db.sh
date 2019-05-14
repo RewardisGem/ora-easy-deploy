65b4f910a2e5690b9f655f020cabf6eb64540d54
Maclean Liu <liu.maclean@gmail.com>
2019-05-14 16:03:57 +0800

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -5,5 +5,28 @@
 
 echo please input DB_NAME/SID 
 read ORACLE_SID
+
+
+echo pls input charset CHARSET
+read CHARSET
+
+
 export ORACLE_SID=$ORACLE_SID
 export TEMPLATE_NAME=General_Purpose.dbc
+export LISTENER_NAME=LISTENER
+export CREATE_AS_CDB=false
+
+export ORACLE_HOME=/opt/oracle/product/19c/dbhome_1
+export PATH=$ORACLE_HOME/bin:$PATH
+LSNR=$ORACLE_HOME/bin/lsnrctl
+SQLPLUS=$ORACLE_HOME/bin/sqlplus
+DBCA=$ORACLE_HOME/bin/dbca
+NETCA=$ORACLE_HOME/bin/netca
+ORACLE_OWNER=oracle
+
+su -s /bin/bash  $ORACLE_OWNER -c "$DBCA -silent -createDatabase -gdbName $ORACLE_SID -templateName $TEMPLATE_NAME -characterSet $CHARSET -createAsContainerDatabase $CREATE_AS_CDB  -createListener $LISTENER_NAME:$LISTENER_PORT -datafileDestination $ORACLE_DATA_LOCATION -sid $ORACLE_SID -autoGeneratePasswords"
+
+
+
+
+
