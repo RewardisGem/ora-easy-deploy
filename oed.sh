19447ec18760098e8007659f8b99a1d717b7349a
Maclean Liu <liu.maclean@gmail.com>
2019-05-15 11:08:42 +0800

    Update oed.sh

--- oed.sh
+++ oed.sh
@@ -1,5 +1,13 @@
 
 
 
+su - root 
 
-wget 
+wget https://www.dropbox.com/s/8097ds292gabpk9/oracle-database-ee-19c-1.0-1.x86_64.rpm
+curl -o oracle-database-preinstall-19c-1.0-1.el7.x86_64.rpm https://yum.oracle.com/repo/OracleLinux/OL7/latest/x86_64/getPackage/oracle-database-preinstall-19c-1.0-1.el7.x86_64.rpm
+
+
+yum -y localinstall oracle-database-preinstall-19c-1.0-1.el7.x86_64.rpm
+
+
+
